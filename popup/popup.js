3337048b9aead727ec5406d9f9237dff6ce646af
vladiroz <vladig87@gmail.com>
2023-07-13 10:08:06 +1000

    popup wip

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,13 @@
 console.log("This is a popup!");
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    const loadingElement = document.getElementById('loading');
+    const popupElement = document.getElementById('popup');
+
+    loadingElement.style.display = 'block';
+    popupElement.style.display = 'none';
+
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         const currentTab = tabs[0];
         const url = currentTab.url;
@@ -17,27 +24,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 const { originalLength, summeryLength, summery } = data;
 
                 loadingElement.style.display = 'none';
-                popupElement.style.display = 'block';
+                popupElement.style.display = 'flex';
 
                 const readingTime = Math.round((originalLength - summeryLength) / 200);
                 document.getElementById('header').innerText = `⏱ saved ${readingTime} min read`;
                 document.getElementById('content').innerText = summery;
-                document.getElementById('footer').style.backgroundColor = '#ccc';
 
             } else if (responseEventType === 'inflight') {
                 console.log('inflight');
             }
         });
     });
-
-    // Get the loading and popup elements
-    const loadingElement = document.getElementById('loading');
-    const popupElement = document.getElementById('popup');
-
-    // Show loading state
-    loadingElement.style.display = 'block';
-    popupElement.style.display = 'none';
-
-
 });
 
